336db30df58ddfe0123c1d861d1c16c077d966d7
Lucas Augusto <nortthon@gmail.com>
2018-09-19 18:22:08 -0300

    add mongo event listener

--- src/main/java/io/github/nortthon/poc/gateways/listeners/MessageEventListener.java
+++ src/main/java/io/github/nortthon/poc/gateways/listeners/MessageEventListener.java
@@ -1,6 +1,5 @@
 package io.github.nortthon.poc.gateways.listeners;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.nortthon.poc.domains.UserEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,23 +7,18 @@ import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Slf4j
 @Component
 public class MessageEventListener {
 
     private final MongoTemplate cosmosMongoTemplate;
 
-    private final ObjectMapper om;
-
-    public MessageEventListener(@Qualifier("cosmosMongoTemplate") final MongoTemplate cosmosMongoTemplate, final ObjectMapper om) {
+    public MessageEventListener(@Qualifier("cosmosMongoTemplate") final MongoTemplate cosmosMongoTemplate) {
         this.cosmosMongoTemplate = cosmosMongoTemplate;
-        this.om = om;
     }
 
     @EventListener
-    public void event(final UserEvent userEvent) throws IOException {
+    public void event(final UserEvent userEvent) {
         cosmosMongoTemplate.getCollection(userEvent.getCollection()).save(userEvent.getDbObject());
     }
 }
